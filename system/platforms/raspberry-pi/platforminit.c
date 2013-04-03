dcce7ee31112f3f0a30d49f1ef0aeddf357f43dc
Jedd Haberstro <jhaberstro@gmail.com>
2013-04-03 17:30:25 -0400

    More not working MMU code.

--- system/platforms/raspberry-pi/platforminit.c
+++ system/platforms/raspberry-pi/platforminit.c
@@ -66,11 +66,16 @@ int platforminit( void )
     //platform.uart_irqnum = 0; /*UART IRQ number? Not read anywhere.*/
 
     typedef struct __attribute__ ((__packed__)) {
-        unsigned start  : 3;
-        unsigned ns     : 2;
-        unsigned domain : 4;
-        unsigned p      : 1;
-        unsigned addr   : 22;
+        union {
+            struct {
+                unsigned start  : 3;
+                unsigned ns     : 2;
+                unsigned domain : 4;
+                unsigned p      : 1;
+                unsigned addr   : 22;
+            };
+            uint32_t value;
+        }
     } coarse_descriptor;
 
     typedef struct __attribute__ ((__packed__)) {
@@ -94,17 +99,29 @@ int platforminit( void )
         small_page_descriptor ptes[256];
     } l2_table;
 
-    static l1_table master;
-    static l2_table tables[512];
+    static l1_table master __attribute__ ((aligned(0x4000)));
+    static l2_table tables[512] __attribute__ ((aligned(0x4000)));
     int coarse, entry;
+    intptr_t currentAddress = 0x0;
     for (coarse = 0; coarse < 512; ++coarse) {
+        l2_table* l2 = &tables[i];
         master.descriptors[coarse].start = 1;
         master.descriptors[coarse].ns = 1;
         master.descriptors[coarse].domain = 0;
         master.descriptors[coarse].p = 1;
-        master.descriptors[coarse].addr = ;
+        master.descriptors[coarse].addr = ((intptr_t)l2) >> 10;
         for (entry = 0; entry < 256; ++entry) {
-
+            l2->ptes[256].xn = 1;  
+            l2->ptes[256].one = 1;  
+            l2->ptes[256].b = 0;  
+            l2->ptes[256].c = 0;  
+            l2->ptes[256].ap = 3;  // kernel read/write ? 
+            l2->ptes[256].tex = 0;  
+            l2->ptes[256].apx = 0;  
+            l2->ptes[256].s = 0;
+            l2->ptes[256].ng = 0;  
+            l2->ptes[256].addr = currentAddress >> 12;
+            currentAddress += 0x4000;
         }
     }
 
@@ -145,23 +162,23 @@ int platforminit( void )
     //    tlb_l1.entries[i].ns = 0;   //
     //    tlb_l1.entries[i].base = i;
     //}
-    //
-    //uint tlb_l1_base = (uint)&tlb_l1;
-    //asm("mcr p15, 0, %0, c2, c0, 0" : : "r" (tlb_l1_base));
+    
+    uint tlb_l1_base = (uint)&master;
+    asm("mcr p15, 0, %0, c2, c0, 0" : : "r" (tlb_l1_base));
 
-    //// section table entry
-    ////uint dest_addr = tlb_l1_base | (0x080 * 4);
-    ////uint entry = tlb_l1_base | 0xc02;
-    ////*(uint*)(dest_addr) = entry;
+    // section table entry
+    //uint dest_addr = tlb_l1_base | (0x080 * 4);
+    //uint entry = tlb_l1_base | 0xc02;
+    //*(uint*)(dest_addr) = entry;
 
-    //// domain AP: "manager" (AP not checked)
-    //asm("mov r0, #0x3");
-    //asm("mcr p15, 0, r0, c3, c0, 0");
+    // domain AP: "manager" (AP not checked)
+    asm("mov r0, #0x3");
+    asm("mcr p15, 0, r0, c3, c0, 0");
 
-    //// enable
-    //asm("mrc p15, 0, r0, c1, c0, 0");
-    //asm("orr r0, r0, #0x1");
-    //asm("mcr p15, 0, r0, c1, c0, 0");
+    // enable
+    asm("mrc p15, 0, r0, c1, c0, 0");
+    asm("orr r0, r0, #0x1");
+    asm("mcr p15, 0, r0, c1, c0, 0");
 
 
     return OK;
