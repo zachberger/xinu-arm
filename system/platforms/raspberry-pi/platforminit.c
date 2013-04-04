bd4fac00befc8e4dcef7d97dcbaa2401125acb84
Kathleen Banawa <kbanawa@gmail.com>
2013-04-04 11:37:53 -0400

    1-1 mapping with coarse & small

--- system/platforms/raspberry-pi/platforminit.c
+++ system/platforms/raspberry-pi/platforminit.c
@@ -12,12 +12,64 @@
 #include "vic.h"
 #include "gpio.h"
 
+#define MAX_SIZE 4096
+
+typedef unsigned long vaddr_t;
+typedef unsigned long paddr_t;
+typedef unsigned long pde_t;
+
+typedef struct coarse {
+    pde_t small[256];
+} coarse_t;
+
 extern ulong cpuid;                     /* Processor id                    */
 extern struct platform platform;        /* Platform specific configuration */
 
 //NOTE on the Raspberry Pi, we rely on the GPU to have initialized the PL011
 //properly so that we can do kprintf since we don't actually initialize it
 //until the latter part of sysinit()
+
+void dump_table(pde_t *table, int start, int end) {
+    int i;
+    for (i = start; i < end; ++i) {
+        kprintf("%0d: %x\r\n", i, table[i]);
+    }
+}
+
+void init_table(coarse_t *pd, vaddr_t virt, paddr_t phys, int size) {
+    volatile coarse_t *ptr; 
+    volatile pde_t entry;
+    int i, count, addr_offset, page_num, table_num;
+    volatile paddr_t phys_base_addr;
+    volatile vaddr_t vbase_addr;
+
+    ptr = (coarse_t*) pd;
+    count = size / 4096; // 256 obv
+    addr_offset = 4096;
+    phys_base_addr = phys; // 1:1 for now, change this later
+
+    for (i = 0; i < count; ++i) {
+        vbase_addr = virt + i * 4096;
+        table_num = vbase_addr / 0x100000;
+        page_num = (vbase_addr & 0xFF000) >> 12;
+
+        entry = phys_base_addr & 0xFFFFF000;
+        entry |= 0x11; // small page number
+        entry |= 0x30;// AP = 3
+
+        (*(ptr + table_num)).small[page_num] = entry;
+
+        phys_base_addr += addr_offset;
+    }
+}
+
+void set_page_mapping(pde_t *pd, vaddr_t virt, paddr_t phys) {
+    // REMOVE{
+    pd[virt >> 20] |= phys & 0xFFF00000;
+    pd[virt >> 20] |= 0x02; // section entry number
+    pd[virt >> 20] |= 0x10; // execute never
+    pd[virt >> 20] |= 0xC00; // AP = 3
+}
 
 /**
  * Determines and stores all platform specific information.
@@ -75,7 +127,7 @@ int platforminit( void )
                 unsigned addr   : 22;
             };
             uint32_t value;
-        }
+        };
     } coarse_descriptor;
 
     typedef struct __attribute__ ((__packed__)) {
@@ -99,31 +151,32 @@ int platforminit( void )
         small_page_descriptor ptes[256];
     } l2_table;
 
-    static l1_table master __attribute__ ((aligned(0x4000)));
-    static l2_table tables[512] __attribute__ ((aligned(0x4000)));
-    int coarse, entry;
+    //static l1_table master __attribute__ ((aligned(0x4000)));
+    //static l2_table tables[4096] __attribute__ ((aligned(0x4000)));
+
+    /*int coarse, entry;
     intptr_t currentAddress = 0x0;
-    for (coarse = 0; coarse < 512; ++coarse) {
-        l2_table* l2 = &tables[i];
+    for (coarse = 0; coarse < 4096; ++coarse) {
+        l2_table *l2 = &tables[i];
         master.descriptors[coarse].start = 1;
-        master.descriptors[coarse].ns = 1;
+        master.descriptors[coarse].ns = 0;
         master.descriptors[coarse].domain = 0;
-        master.descriptors[coarse].p = 1;
+        master.descriptors[coarse].p = 0;
         master.descriptors[coarse].addr = ((intptr_t)l2) >> 10;
         for (entry = 0; entry < 256; ++entry) {
-            l2->ptes[256].xn = 1;  
-            l2->ptes[256].one = 1;  
-            l2->ptes[256].b = 0;  
-            l2->ptes[256].c = 0;  
-            l2->ptes[256].ap = 3;  // kernel read/write ? 
-            l2->ptes[256].tex = 0;  
-            l2->ptes[256].apx = 0;  
-            l2->ptes[256].s = 0;
-            l2->ptes[256].ng = 0;  
-            l2->ptes[256].addr = currentAddress >> 12;
+            l2->ptes[entry].xn = 1;  
+            l2->ptes[entry].one = 1;  
+            l2->ptes[entry].b = 0;  
+            l2->ptes[entry].c = 0;  
+            l2->ptes[entry].ap = 0;  // kernel read/write ? 
+            l2->ptes[entry].tex = 0;  
+            l2->ptes[entry].apx = 0;  
+            l2->ptes[entry].s = 0;
+            l2->ptes[entry].ng = 0;  
+            l2->ptes[entry].addr = ((intptr_t)l2) >> 12;
             currentAddress += 0x4000;
         }
-    }
+    }*/
 
     //static struct __attribute__ ((__packed__)) {
     //    struct __attribute__ ((__packed__)) {
@@ -162,14 +215,34 @@ int platforminit( void )
     //    tlb_l1.entries[i].ns = 0;   //
     //    tlb_l1.entries[i].base = i;
     //}
-    
-    uint tlb_l1_base = (uint)&master;
-    asm("mcr p15, 0, %0, c2, c0, 0" : : "r" (tlb_l1_base));
 
-    // section table entry
-    //uint dest_addr = tlb_l1_base | (0x080 * 4);
-    //uint entry = tlb_l1_base | 0xc02;
-    //*(uint*)(dest_addr) = entry;
+    static pde_t master[MAX_SIZE] __attribute__ ((aligned(0x4000)));
+    static coarse_t l2[MAX_SIZE] __attribute__ ((aligned(0x400)));
+    volatile pde_t entry;
+    int j;
+
+    // REMOVE: 1:1 section table
+    //for (j = 0; j < MAX_SIZE; ++j) {
+    //    set_page_mapping(master, j << 20, j << 20);
+    //}
+
+    for (j = 0; j < MAX_SIZE; ++j) {
+        entry = (((uint)l2 + (uint)j*1024) & 0xFFFFFC00);
+        entry |= 0x1;
+        init_table(l2, j << 20, j << 20, 0x100000);
+        master[j] = entry;
+    }
+
+    // REMOVE: debug
+    dump_table(master, 0, 20);
+    
+    // REMOVE: coarse table at first 1 MB of master
+    //init_table(l2, 0, 0, 0x100000);
+    //master[0] = ((uint)l2 & 0xFFFFFC00);
+    //master[0] |= 0x1;
+
+    //uint tlb_l1_base = (uint)&master;
+    asm("mcr p15, 0, %0, c2, c0, 0" : : "r" (master) : "memory");
 
     // domain AP: "manager" (AP not checked)
     asm("mov r0, #0x3");
@@ -179,7 +252,6 @@ int platforminit( void )
     asm("mrc p15, 0, r0, c1, c0, 0");
     asm("orr r0, r0, #0x1");
     asm("mcr p15, 0, r0, c1, c0, 0");
-
-
+    
     return OK;
 }
