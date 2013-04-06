65bade35500fa3faeed08d797b8edca278637316
Jedd Haberstro <jhaberstro@gmail.com>
2013-04-06 15:07:12 -0400

    Added arm platform directory structure.

---
+++ system/platforms/arm/mmu.h
@@ -0,0 +1,13 @@
+/**
+ * ARMv6 4k page Virtual Memory Manager
+ *
+ * Date: April 6, 2013
+ * Author(s): Kathleen Banawa, Jedd Haberstro
+ */
+
+#include <stdint.h>
+
+typedef uintptr_t vaddr_t;
+typedef uintptr_t paddr_t;
+typedef uintptr_t pde_t;
+
