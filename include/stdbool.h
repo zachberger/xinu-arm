0cd9a644fdd1290de80da7a46b677e78163a1e0d
Jedd Haberstro <jhaberstro@gmail.com>
2013-04-29 23:58:24 -0400

    Added stdbool.h

---
+++ include/stdbool.h
@@ -0,0 +1,11 @@
+#ifndef _STDBOOL_H_
+#define _STDBOOL_H_
+
+#ifndef bool
+    #define bool int
+    #define true 1
+    #define false 0
+    #define __bool_true_false_are_defined 1
+#endif
+
+#endif // _STDBOOL_H_
