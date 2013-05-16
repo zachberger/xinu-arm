f74b980b2d6e2d8c3ea76d58f2fcdeabe488b844
Eric Warrington <esw5886@hendrix.cs.rit.edu>
2013-05-15 21:46:51 -0400

    voltage and temperature properties added

--- system/platforms/raspberry-pi/properties.c
+++ system/platforms/raspberry-pi/properties.c
@@ -19,33 +19,42 @@ mem_region get_vc_memory()
 	volatile u32* buf=(u32*)BUFFER_ADDRESS;	//u32 addr, size;	buf[0] = 8 * 4; /* buffer size */	buf[1] = 0; /* request/response code */	/* tag - get VC memory*/	buf[2] = 0x00010006; /* tag id */	buf[3] = 8; /* value buffer size */	buf[4] = 0+0; /* request indicator + size */	buf[5] = 0; /* req: mem address */	buf[6] = 0; /* req: mem size */	buf[7] = 0; /* end tags */	mailbox_write(MAIL_CHAN_ARMTOVC_PROP, (u32)buf);	mailbox_read(MAIL_CHAN_ARMTOVC_PROP);	if(buf[1]!=MAIL_PROP_RESPONSE_OK)	{		fb_error();		return;	}	MR.base_address=buf[5];	MR.block_size=buf[6];
 }
 
-u32 get_temperature();
-u32 get_max_temperature();
 
 
-/**
- * Device IDs
- */
-#define RESERVED 0x000000000
-#define CORE 	 0x000000001
-#define SDRAM_C  0x000000002
-#define SDRAM_P  0x000000003
-#define SDRAM_I  0x000000004
-
-u32 get_voltage( u32 device_id );
-u32 get_max_voltage( u32 device_id );
-u32 set_voltage( u32 device_id, u32 voltage );
 
 
-#define SD_CARD 0x00000000
-#define UART0 	0x00000001
-#define UART1   0x00000002
-#define USB_HCD 0x00000003
-#define I2C0 	0x00000004
-#define I2C1 	0x00000005
-#define I2C2 	0x00000006 
-#define SPI 	0x00000007
-#define CCP2TX 	0x00000008
+
+u32 get_temperature()
+{
+	volatile u32* buf=(u32*)BUFFER_ADDRESS;	buf[0] = 7*4; /* buffer size */	buf[1] = 0; /* request/response code */	/* tag - get temperature*/	buf[2] = 0x00030006; /* tag id */	buf[3] = 8; /* value buffer size */	buf[4] = 0+0; /* request indicator + size */	buf[5] = 0; /* req: temperature id */	buf[6] = 0; /* req: value */	buf[7] = 0; /* end tags */	mailbox_write(MAIL_CHAN_ARMTOVC_PROP, (u32)buf);	mailbox_read(MAIL_CHAN_ARMTOVC_PROP);	if(buf[1]!=MAIL_PROP_RESPONSE_OK)	{		fb_error();		return;	}	return buf[6];
+}
+
+u32 get_max_temperature()
+{
+	volatile u32* buf=(u32*)BUFFER_ADDRESS;	//u32 addr, size;	buf[0] = 7*4; /* buffer size */	buf[1] = 0; /* request/response code */	/* tag - get max temperature*/	buf[2] = 0x0003000A; /* tag id */	buf[3] = 8; /* value buffer size */	buf[4] = 0+0; /* request indicator + size */	buf[5] = 0; /* req: temperature id */	buf[6] = 0; /* req: value */	buf[7] = 0; /* end tags */	mailbox_write(MAIL_CHAN_ARMTOVC_PROP, (u32)buf);	mailbox_read(MAIL_CHAN_ARMTOVC_PROP);	if(buf[1]!=MAIL_PROP_RESPONSE_OK)	{		fb_error();		return;	}	return buf[6];
+}
+
+
+
+
+//(offset from 1.2V in units of 0.025V)
+u32 get_voltage(u32 device_id)
+{
+	volatile u32* buf=(u32*)BUFFER_ADDRESS;	buf[0] = 7*4; /* buffer size */	buf[1] = 0; /* request/response code */	/* tag - get voltage*/	buf[2] = 0x00030003; /* tag id */	buf[3] = 8; /* value buffer size */	buf[4] = 0+0; /* request indicator + size */	buf[5] = device_id; /* req: voltage id */	buf[6] = 0; /* req: value (offset from 1.2V in units of 0.025V) */	buf[7] = 0; /* end tags */	mailbox_write(MAIL_CHAN_ARMTOVC_PROP, (u32)buf);	mailbox_read(MAIL_CHAN_ARMTOVC_PROP);	if(buf[1]!=MAIL_PROP_RESPONSE_OK)	{		fb_error();		return;	}	return buf[6];
+}
+
+//Return the maximum supported voltage rate for the given id. Voltages should not be set higher than this. 
+u32 get_max_voltage(u32 device_id)
+{
+	volatile u32* buf=(u32*)BUFFER_ADDRESS;	buf[0] = 7*4; /* buffer size */	buf[1] = 0; /* request/response code */	/* tag - get max voltage*/	buf[2] = 0x00030005; /* tag id */	buf[3] = 8; /* value buffer size */	buf[4] = 0+0; /* request indicator + size */	buf[5] = device_id; /* req: voltage id */	buf[6] = 0; /* req: value (offset from 1.2V in units of 0.025V) */	buf[7] = 0; /* end tags */	mailbox_write(MAIL_CHAN_ARMTOVC_PROP, (u32)buf);	mailbox_read(MAIL_CHAN_ARMTOVC_PROP);	if(buf[1]!=MAIL_PROP_RESPONSE_OK)	{		fb_error();		return;	}	return buf[6];
+}
+
+u32 set_voltage(u32 device_id, u32 voltage)
+{
+	volatile u32* buf=(u32*)BUFFER_ADDRESS;	buf[0] = 7*4; /* buffer size */	buf[1] = 0; /* request/response code */	/* tag - set voltage*/	buf[2] = 0x00038003; /* tag id */	buf[3] = 8; /* value buffer size */	buf[4] = 0+0; /* request indicator + size */	buf[5] = device_id; /* req: voltage id */	buf[6] = voltage; /* req: value (offset from 1.2V in units of 0.025V) */	buf[7] = 0; /* end tags */	mailbox_write(MAIL_CHAN_ARMTOVC_PROP, (u32)buf);	mailbox_read(MAIL_CHAN_ARMTOVC_PROP);	if(buf[1]!=MAIL_PROP_RESPONSE_OK)	{		fb_error();		return;	}	return buf[6];
+}
+
+
 
 u32 get_device_state( u32 device_id );
 u32 get_device_timing( u32 device_id );
